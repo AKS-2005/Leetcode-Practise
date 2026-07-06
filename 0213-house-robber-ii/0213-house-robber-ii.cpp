6e501338770a1dd3d71c03f6f6180846f3958f73
AKS-2005 <saxena.akshiv@gmail.com>
2026-07-06 22:10:18 +0530

    Time: 3 ms (7.01%), Space: 10.9 MB (18.81%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+
+        int case1 = robb(nums, 0, n - 2, dp1);
+        int case2 = robb(nums, 1, n - 1, dp2);
+
+        return max(case1, case2);
+    }
+
+    int robb(vector<int>& nums, int index, int end, vector<int>& dp) {
+        if (index > end) return 0;
+
+        if (dp[index] != -1) return dp[index];
+
+        int pick = nums[index] + robb(nums, index + 2, end, dp);
+        int notPick = robb(nums, index + 1, end, dp);
+
+        return dp[index] = max(pick, notPick);
+    }
+};
