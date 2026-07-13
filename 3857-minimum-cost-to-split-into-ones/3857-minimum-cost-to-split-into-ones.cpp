204bc02693f035f2d26ea94c1e1c8262f9de1257
AKS-2005 <saxena.akshiv@gmail.com>
2026-07-14 00:56:36 +0530

    Time: 158 ms (5.05%), Space: 10.6 MB (12.15%) - LeetHub

---
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(int n){
+        if(n==1){
+            return 0;
+        }
+        int ans = INT_MAX;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        for(int i =1;i<n;i++){
+            int b= n-i;
+            ans = min(ans,i*b+solve(i)+solve(b));
+        }
+        return dp[n]=ans;
+    }
+    int minCost(int n) {
+        dp.assign(n+1,-1);
+        return solve(n);
+    }
+};
