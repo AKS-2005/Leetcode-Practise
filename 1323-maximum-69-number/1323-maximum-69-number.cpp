08a27597011f04f085f37a855b772255b88af1b0
AKS-2005 <saxena.akshiv@gmail.com>
2026-07-18 23:51:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (8.03%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    int maximum69Number (int num) {
-        string s=to_string(num);
+    int maximum69Number(int num) {
+        /*string s=to_string(num);
         for(int i=0;i<s.length();i++){
             if(s[i]=='6'){
                 s[i]='9';
                 break;
             }
         }
-        return stoi(s);
+        return stoi(s);*/
+        vector<int> arr;
+        while (num > 0) {
+            int digi = num % 10;
+            arr.push_back(digi);
+            num /= 10;
+        }
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            if (arr[i] == 6) {
+                arr[i] = 9;
+                break;
+            }
+        }
+        int res = 0;
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            res = res * 10 + arr[i];
+        }
+        return res;
     }
 };
