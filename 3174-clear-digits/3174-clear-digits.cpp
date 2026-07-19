f9a3df0ed7f14d7ee8b63ceffa4cd1217e54ff3d
AKS-2005 <saxena.akshiv@gmail.com>
2026-07-19 16:39:50 +0530

    Time: 2 ms (17.71%), Space: 9.1 MB (35.91%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(char ch:s){
+            if(isalpha(ch)){
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+        }
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
