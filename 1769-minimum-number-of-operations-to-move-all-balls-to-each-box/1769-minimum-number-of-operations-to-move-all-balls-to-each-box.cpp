7cda0996e0a495e9e7e0db76a6f6a230d93280cb
AKS-2005 <saxena.akshiv@gmail.com>
2026-09-04 13:47:52 +0530

    Time: 0 ms (100%), Space: 12.5 MB (43.78%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int n=boxes.size();
+        /*int n=boxes.size();      -----> BRUTE FORCE
         vector<int> ans(n,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -9,6 +9,26 @@ public:
                     ans[i]+=abs(i-j);
                 }
             }
+        }
+        return ans;*/
+        int n=boxes.size();
+        int sum=0;
+        int one=0;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            sum +=one;
+            if(boxes[i]=='1'){
+                one++;
+            }
+            ans.push_back(sum);
+        }
+        sum=0;one=0;
+        for(int i=n-1;i>=0;i--){
+            sum +=one;
+            if(boxes[i]=='1'){
+                one++;
+            }
+            ans[i]+=sum;
         }
         return ans;
     }
