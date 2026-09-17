76bff0ca1cfbfb1dc53af00cef483d7cba138496
AKS-2005 <saxena.akshiv@gmail.com>
2026-09-17 23:01:17 +0530

    Time: 3 ms (15.54%), Space: 27.1 MB (32.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-        int l=0;
+        /*int l=0;
         int r=arr.size()-1;
         int lm=arr[l];
         int rm=arr[r];
@@ -18,6 +18,21 @@ public:
                 wt+=rm-arr[r];
             }
         }
-        return wt;
+        return wt;*/
+        stack<int> st;
+        int ans=0;
+        for(int i=0;i<arr.size();i++){
+            while(!st.empty() && arr[i]>arr[st.top()]){
+                int bottom=st.top();
+                st.pop();
+                if(st.empty()) break;
+                int l=st.top();
+                int w=i-l-1;
+                int h=min(arr[l],arr[i])-arr[bottom];
+                ans+=w*h;
+            }
+            st.push(i);
+        }
+        return ans;
     }
 };
