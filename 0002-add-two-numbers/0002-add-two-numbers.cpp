bc7ad69a0af80b13662206a8c8d58b8f95608c1c
AKS-2005 <saxena.akshiv@gmail.com>
2026-09-17 18:02:56 +0530

    Time: 0 ms (100%), Space: 77.1 MB (45.96%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,14 +14,13 @@ public:
         ListNode* dummy = new ListNode();
         ListNode* res = dummy;
         int total = 0, carry = 0;
-
         while (l1 || l2 || carry) {
             total = carry;
-
             if (l1) {
                 total += l1->val;
                 l1 = l1->next;
             }
+    
             if (l2) {
                 total += l2->val;
                 l2 = l2->next;
@@ -32,7 +31,6 @@ public:
             dummy->next = new ListNode(num);
             dummy = dummy->next;
         }
-
         ListNode* result = res->next;
         delete res;
         return result; 
